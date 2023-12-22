1f3d52daf0a4964efdf60bdd4846720b7fd61e52
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2023-12-22 17:57:39 +0100

    Use CEu Buildscript

--- README.md
+++
@@ -1,3 +0,0 @@
-## GSECore
-
-Core mod for Gregtech: Skyblock Edition. Currently only modifies some ExNihilo stuff.
---
+++ src/main/java/com/irgendwer01/gsecore/GSECore.java
@@ -0,0 +1,6 @@
+package com.irgendwer01.gsecore;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Tags.MODID, name = Tags.MODNAME, version = Tags.VERSION)
+public class GSECore {}
