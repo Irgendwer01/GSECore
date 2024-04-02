965cd93724a688524ff1b964232b09d214708d35
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2024-04-02 14:37:04 +0200

    Add Gregblock stone gen

--- src/main/java/com/irgendwer01/gsecore/EventBusSubscriber.java
+++ src/main/java/com/irgendwer01/gsecore/EventBusSubscriber.java
@@ -3,10 +3,17 @@ package com.irgendwer01.gsecore;
 import static gregtech.api.unification.ore.OrePrefix.Conditions.hasOreProperty;
 import static gregtech.api.unification.ore.OrePrefix.Flags.ENABLE_UNIFICATION;
 
+import net.minecraft.block.BlockStone;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import exnihilocreatio.ModFluids;
 import gregtech.api.unification.material.Materials;
 import gregtech.api.unification.material.event.MaterialEvent;
 import gregtech.api.unification.material.info.MaterialIconType;
@@ -45,5 +52,35 @@ public class EventBusSubscriber {
         oreNetherChunk.setAlternativeOreName(OrePrefix.oreNetherrack.name());
 
         MetaItems.addOrePrefix(oreChunk, oreEnderChunk, oreNetherChunk);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public static void stoneGen(BlockEvent.FluidPlaceBlockEvent event) {
+        if (event.getOriginalState().getBlock() == Blocks.FLOWING_LAVA) {
+            BlockPos lavaPos = event.getLiquidPos(), witchWaterPos = null;
+            World world = event.getWorld();
+            for (BlockPos pos : new BlockPos[]{lavaPos.east(), lavaPos.west(), lavaPos.north(), lavaPos.south()})
+                if (world.getBlockState(pos).getBlock() == ModFluids.blockWitchwater) {
+                    witchWaterPos = pos;
+                    break;
+                }
+            if (witchWaterPos != null) {
+                switch (world.getBlockState(witchWaterPos).getBlock().getMetaFromState(world.getBlockState(witchWaterPos))) {
+                    default:
+                        event.setNewState(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE));
+                        break;
+                    case 2:
+                    case 3:
+                        event.setNewState(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE));
+                        break;
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                        event.setNewState(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.ANDESITE));
+                        break;
+                }
+            }
+        }
     }
 }
