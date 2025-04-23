c422b9c11f506d9fa64c0a2c5ca7eadb79b30c0a
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2025-04-24 01:54:37 +0200

    Allow Gas Collector to work in Personal Dims

---
+++ src/main/java/com/irgendwer01/gsecore/mixins/gregtech/MixinMetaTileEntityGasCollector.java
@@ -0,0 +1,21 @@
+package com.irgendwer01.gsecore.mixins.gregtech;
+
+import gregtech.common.metatileentities.electric.MetaTileEntityGasCollector;
+import net.minecraft.world.WorldProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import personalworlds.world.PWWorldProvider;
+
+@Mixin(MetaTileEntityGasCollector.class)
+public class MixinMetaTileEntityGasCollector {
+
+    @Redirect(method = "checkRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldProvider;getDimension()I"))
+    private int getDimension(WorldProvider instance) {
+        if (instance instanceof PWWorldProvider) {
+            return 0;
+        } else {
+            return instance.getDimension();
+        }
+    }
+}
