b2f0eb7776c4d0becb50d17d469a3d501859a6e8
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2025-05-02 03:17:17 +0200

    Spotless

--- src/main/java/com/irgendwer01/gsecore/mixins/gregtech/MixinMetaTileEntityGasCollector.java
+++ src/main/java/com/irgendwer01/gsecore/mixins/gregtech/MixinMetaTileEntityGasCollector.java
@@ -1,16 +1,19 @@
 package com.irgendwer01.gsecore.mixins.gregtech;
 
-import gregtech.common.metatileentities.electric.MetaTileEntityGasCollector;
 import net.minecraft.world.WorldProvider;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import gregtech.common.metatileentities.electric.MetaTileEntityGasCollector;
 import personalworlds.world.PWWorldProvider;
 
 @Mixin(value = MetaTileEntityGasCollector.class, remap = false)
 public class MixinMetaTileEntityGasCollector {
 
-    @Redirect(method = "checkRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldProvider;getDimension()I"))
+    @Redirect(method = "checkRecipe",
+              at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldProvider;getDimension()I"))
     private int getDimension(WorldProvider instance) {
         if (instance instanceof PWWorldProvider) {
             return 0;
